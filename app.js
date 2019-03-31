7f62e05f40e3e5540b73306252a657637b2c999b
Misty Dawn Hall <mistydawncarmichael@gmail.com>
2019-03-31 13:08:43 -0400

    parsed pertinent data

--- app.js
+++ app.js
@@ -48,13 +48,18 @@ fs.readFile(__dirname + '/samples/nmap.results.xml', function(err, data) {
         hostname = scan.hostnames[0];
       }
       const portID1 = scan.ports[0].port[0]['$'].portid;
+      const portID2 = scan.ports[0].port[1]['$'].portid;
+      const portID3 = scan.ports[0].port[2]['$'].portid;
       const protocol1 = scan.ports[0].port[0]['$'].protocol;
+      const protocol2 = scan.ports[0].port[1]['$'].protocol;
+      const protocol3 = scan.ports[0].port[2]['$'].protocol;
       const hostStatus = scan.status[0]['$'].state;
       const portState1 = scan.ports[0].port[0].state[0]['$'].state;
-      // const statusStateReason = scan.status[0]['$'].reason;
-      // const portStateReason = scan.ports[0].port[0].state[0]['$'].reason;
-      // const statusStateReasonTTL = scan.status[0]['$'].reason_ttl;
-      // const portStateReasonTTL = scan.ports[0].port[0].state[0]['$'].reason_ttl;
+      const portState2 = scan.ports[0].port[1].state[0]['$'].state;
+      const portState3 = scan.ports[0].port[2].state[0]['$'].state;
+      const service1 = scan.ports[0].port[0].service[0]['$'].name;
+      const service2 = scan.ports[0].port[1].service[0]['$'].name;
+      const service3 = scan.ports[0].port[2].service[0]['$'].name;
       const startTime = scan.$.starttime;
       const stopTime = scan.$.endtime;
 
@@ -63,43 +68,42 @@ fs.readFile(__dirname + '/samples/nmap.results.xml', function(err, data) {
       console.log(`hostname is ${JSON.stringify(hostname)}`);
       console.log(`host status is ${hostStatus}`);
       console.log(`port_id 1 is ${portID1}`);
-      //
-      //
+      console.log(`port_id 2 is ${portID2}`);
+      console.log(`port_id 3 is ${portID3}`);
       console.log(`protocol 1 is ${protocol1}`);
-      // 
-      //
+      console.log(`protocol 2 is ${protocol2}`);
+      console.log(`protocol 3 is ${protocol3}`);
       //
       console.log(`port state 1 is ${portState1}`);
-      //
-      //
-      // service 1
-      //
-      //
+      console.log(`port state 2 is ${portState2}`);
+      console.log(`port state 3 is ${portState3}`);
+      console.log(`service 1 is ${service1}`);
+      console.log(`service 2 is ${service2}`);
+      console.log(`service 3 is ${service3}`);
       console.log(`host start time is ${startTime}`);
       console.log(`host stop time is ${stopTime}`);
-
-
-      // X console.log(`port state reason_ttl is ${portStateReasonTTL}`);
-      // X console.log(`status state reason_ttl is ${statusStateReasonTTL}`);
-      // X console.log(`port state reason is ${portStateReason}`);
-      // X console.log(`status state reason is ${statusStateReason}`);
       
       
 
-      /* writeScanToDB({
+      writeScanToDB({
         'ip': myIP,
         'hostname': hostname,
-        'port_id': portID,
-        'protocol': protocol,
-        'status state': statusState,
-        'port state': portState,
-        'status state reason': statusStateReason,
-        'port state reason': portStateReason,
-        'status state reason_ttl': statusStateReasonTTL,
-        'port state reason_ttl': portStateReasonTTL,
+        'host status': hostStatus,
+        'port_id 1': portID1,
+        'port_id 2': portID2,
+        'port_id 3': portID3,
+        'protocol 1': protocol1,
+        'protocol 2': protocol1,
+        'protocol 3': protocol1,
+        'port state 1': portState1,
+        'port state 2': portState2,
+        'port state 3': portState3,
+        'service 1': service1,
+        'service 2': service2,
+        'service 3': service3,
         'host start time': startTime,
         'host stop time': stopTime
-      });*/
+      });
     } 
     
       
